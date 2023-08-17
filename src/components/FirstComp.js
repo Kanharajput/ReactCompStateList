f0a71104d485208b01d23f7b7c064fa63f1dc725
Kanha Tomar <kanharajput91@gmail.com>
2023-08-17 12:35:13 +0530

    adding css to a component

--- src/components/FirstComp.js
+++ src/components/FirstComp.js
@@ -1,7 +1,10 @@
+// import css file
+import './FirstComp.css';
+
 function FirstComp(){
     return(
         // return a only single element
-        <div>
+        <div className="blog">
             <h1>Kanha's Blog</h1>
             <p>Today I wanna eat some tasty.</p>
             <p>God bless me with some tasty food</p>
