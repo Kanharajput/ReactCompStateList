a64dcd73877dfefd36171db60f57b7abde8e89e7
Kanha Tomar <kanharajput91@gmail.com>
2023-08-17 19:35:44 +0530

    separte the date in month,year & day

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,17 @@ import './ExpenseItem.css';
 // all data which we sended by the app component is gathered in one object called properties(props)
 // extract the desired data
 function ExpenseItem(props){
+    const year = props.date.getFullYear();
+    const month = props.date.toLocaleString('en-US',{month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     return(
         // return a only single element
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
