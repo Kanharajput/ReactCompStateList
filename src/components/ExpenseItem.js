a7d88d34fd4d5c557da4ce71e345f7bc77d971ed
Kanha Tomar <kanharajput91@gmail.com>
2023-08-17 20:05:12 +0530

    multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,14 @@
 // import css file
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 // all data which we sended by the app component is gathered in one object called properties(props)
 // extract the desired data
 function ExpenseItem(props){
-    const year = props.date.getFullYear();
-    const month = props.date.toLocaleString('en-US',{month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     return(
         // return a only single element
         <div className="expense-item">
-            <div>
-                <div>{day}</div>
-                <div>{month}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
