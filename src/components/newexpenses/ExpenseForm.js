a0995fb23703126a8e98a5bf75f36da68a8922f6
Kanha Tomar <kanharajput91@gmail.com>
2023-08-18 17:35:34 +0530

    mulitple states in one component

--- src/components/newexpenses/ExpenseForm.js
+++ src/components/newexpenses/ExpenseForm.js
@@ -1,26 +1,41 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm(){
+    // states to store the user inputs
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmout, setEnteredAmout] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
     // react js pass event to this function 
     // so that we can tackle the 
     const titleHandler = (event) =>{
         console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    }
+    const AmoutHandler = (event) =>{
+        console.log(event.target.value);
+        setEnteredAmout(event.target.value);
+    }
+    const DateHandler = (event) =>{
+        console.log(event.target.value);
+        setEnteredDate(event.target.value);
     }
 
     return(
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
-                    <label for="title-field">Enter title</label>
+                    <label htmlFor="title-field">Enter title</label>
                     <input name="title-field" type="text" onChange={titleHandler}/>
                 </div>
                 <div className="new-expense__control">
-                    <label for="date-field">Enter Amout</label>
-                    <input name="date-field" type="number" />
+                    <label htmlFor="date-field">Enter Amout</label>
+                    <input name="date-field" type="number" onChange={AmoutHandler} />
                 </div>
                 <div className="new-expense__control">
-                    <label for="date-field">Enter Date</label>
-                    <input name="date-field" type="date" />
+                    <label htmlFor="date-field">Enter Date</label>
+                    <input name="date-field" type="date" onChange={DateHandler} />
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">Add Expense</button>
