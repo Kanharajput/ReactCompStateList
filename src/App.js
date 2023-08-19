e1a32ef7b39e5d6a65706d62ca71e11aa86505ad
Kanha Tomar <kanharajput91@gmail.com>
2023-08-19 12:24:01 +0530

    added the newExpense to Expense but now rendering list needed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import ExpenseItem from './components/ExpenseItem';
 import Card from './components/Card';
 import NewExpense from './components/newexpenses/NewExpense';
+import {useState} from 'react';
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -27,12 +28,19 @@ function App() {
       amount: 450,
       date: new Date(2023, 8, 17)
     },
-  ];
+  ]);
 
   const getExpenseFromNewExpense = (expense) => {
-    console.log(expense);
+    // convert date string to date object
+    let date = new Date(expense.date);
+    // replace date string in expense object
+    expense.date = date;
+    setExpenses(prevExpenses => prevExpenses.push(expense));
+    console.log(expenses);
   }
 
+  // now we need code to render the expenses as expenses are stored in list.
+  // below code won't work for that
   return (
     <Card>
       <NewExpense passExpense={getExpenseFromNewExpense}/>
