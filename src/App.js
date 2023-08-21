e009212141b7933351cd9c675b6fd3193db48485
Kanha Tomar <kanharajput91@gmail.com>
2023-08-21 11:41:24 +0530

    change the struc data in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import Expense from './components/middleexpense/Expense';
+import NewExpense from './components/newexpenses/NewExpense';
+import {useState} from 'react';
 
-// removed all from here as App is a root component 
-// so don't throw all inside it.
-function App() {  
+const initial_expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2023, 8, 17)
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2023, 8, 17)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2023, 8, 17)
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2023, 8, 17)
+  }
+];
+
+function App() { 
+  const [expenses, setExpenses] = useState(initial_expenses); 
+  
+  const getExpenseFromNewExpense = (expense) => {
+    setExpenses([expense, ...expenses]);
+  }
+  
   return(
-    <Expense />
+    <div>
+      <NewExpense passExpense={getExpenseFromNewExpense} />
+      <Expense items={expenses} />
+    </div>
   );
 }
 
